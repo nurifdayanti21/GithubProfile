97d53b3e9ec52f23601c2ca385bdf6659d3c850f
nurifdayanti <nurifdayanti710@gmail.com>
2024-12-09 15:46:28 +0700

    first commit

---
+++ app/src/main/java/id/ac/polbeng/nurifdayanti/githubprofile/activities/MyAppGlideModule.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.nurifdayanti.githubprofile.activities
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
